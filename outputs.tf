1fd639df6c590d780348e2cc20a43cd4086b6b8f
eddievaltix <61066726+eddievaltix@users.noreply.github.com>
2021-04-07 21:08:28 -0700

    Update outputs.tf
    Removed firewall role.. This is for the discovery IAM controller role only

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "valtix_controller_role_arn" {
   description = "this outputs the valtix-controller IAM role ARN"
   value       = aws_iam_role.valtix_controller_role.arn
 }
-
-output "valtix_firewall_role_name" {
-  description = "this outputs the name of the Valtix firewall role"
-  value       = aws_iam_role.valtix_fw_role.name
-}
